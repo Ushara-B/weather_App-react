8a19239888d897c53c2c1a8f2f8ccf6635aaa987
Ushara Wijesinghe <127342854+Ushara-B@users.noreply.github.com>
2024-03-18 14:03:17 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import WeatherApp from './components/weatherApp/weatherApp';  // Update import statement
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp />  {/* Use PascalCase for component names */}
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
